ee553ee07fecc57cc76fa3ccce3b4d944522a889
ptats <tacjka34@gmail.com>
2022-07-07 12:57:50 +0200

    Setup cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import config from './config';
@@ -21,11 +22,7 @@ mongoose
   .catch((error) => console.log(error.reason));
 
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/uploads', uploadRoute);
 app.use('/api/users', userRoute);
